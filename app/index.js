44fa9b76202d7de93060f693b60a983056c4b030
michaelwong2 <michaelwongait@gmail.com>
2017-06-19 15:31:07 -0400

    components fixed

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
+
 import React from 'react';
 import { render } from 'react-dom';
 
 import { Nav } from "./components/Nav";
+import { Main } from "./components/Main";
+import {Bio} from "./components/Bio";
 
 render(<Nav/>, window.document.getElementById("nav"));
+render(<Main/>, window.document.getElementById("main"));
