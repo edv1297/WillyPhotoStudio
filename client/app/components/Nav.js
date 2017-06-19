a8ec50b51fffbd3987f33a75b406a9e28438836b
edv1297 <edv2@williams.edu>
2017-06-19 15:16:32 -0400

    Some text is there, not really centered or working yet :(

---
+++ client/app/components/Nav.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export class Nav extends React.Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			collapsed: false,
+			classes: ["navigator", "navigator-full"],
+		};
+
+	}
+
+	componentDidMount(){
+		window.addEventListener('scroll', this.handleScroll.bind(this));
+	}
+
+	componentWillUnmount() {
+    	window.removeEventListener('scroll', this.handleScroll.bind(this));
+  	}
+
+	handleScroll(){
+		if(!this.state.collapsed && window.scrollY >= 100){
+			window.document.getElementById("logo-main-img").src = "assets/logoblack.png";
+			startAnimation(this, "navbar-collapse", ["navigator", "navigator-small"], 500);
+		}else if(this.state.collapsed && window.scrollY < 100){
+			window.document.getElementById("logo-main-img").src = "assets/logowhite.png";
+			startAnimation(this, "navbar-open", ["navigator", "navigator-full"], 500);
+		}else return;
+
+		this.state.collapsed = !this.state.collapsed;
+	}
+
+	render(){
+		return(
+			<div className={this.state.classes.join(" ")} role="navigation">
+				<div className="navbar-tab" style={{left: 150 + 'px'}}>link</div>
+				<div className="navbar-tab" style={{left: 270 + 'px'}}>link</div>
+				<div className="navbar-tab" style={{left: 390 + 'px'}}>link</div>
+				<div id="logo-main">
+					<img height="30" width="150" src="assets/logowhite.png" id="logo-main-img"/>
+				</div>
+				<div className="navbar-tab" style={{left: 780 + 'px'}}>link</div>
+				<div className="navbar-tab" style={{left: 900 + 'px'}}>link</div>
+				<div className="navbar-tab" style={{left: 1020 + 'px'}}>link</div>
+			</div>
+		);
+	}
+}
+
+
+function startAnimation(object, keyframes, finishedState, duration){
+	object.state.classes.push(keyframes);
+	object.setState({
+		classes: object.state.classes
+	});
+
+	setTimeout(() => {
+		object.setState({classes: finishedState});
+	}, duration);
+}
