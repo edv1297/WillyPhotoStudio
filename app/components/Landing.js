7166f0792afddd068f8417603e7ece2c7e07e53b
edv1297 <edv2@williams.edu>
2017-06-21 14:08:59 -0400

    landing page is silver, not gold yet, missing simple image editing

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 import { Nav } from "./Nav";
-import {Footer} from './Footer';
+import {Footer} from "./Footer";
 
 export class Landing extends React.Component{
     constructor(props){
@@ -17,7 +17,7 @@ export class Landing extends React.Component{
       window.removeEventListener("resize", (() => {this.setState({})}).bind(this));
     }
 
-    render(){      
+    render(){
       return(
         <div>
             <Nav collapsable={true}/>
@@ -42,21 +42,39 @@ export class Landing extends React.Component{
 
 export const MainLinks = (props) => {
     return(
-        <div className="blurb-box">
-          <div className="main-link-button">gallery</div>
-          <div className="main-link-button">blog</div>
-          <div className="main-link-button">support</div>
-          <div className="main-link-button">services</div>
-        </div>
+        <div className="blurb-box" style={{height: 'auto'}}>
+
+          <div className="main-link-button">
+            <img src = "./assets/gallery.png" style = {{width:'25vw'}}/>
+            <div id = "img-text">gallery</div>
+
+          </div>
+
+          <div className="main-link-button">
+            <img src = "./assets/frontFacing/pot.png" style = {{width:'25vw'}}/>
+            <div id = "img-text">blog</div>
+          </div>
+
+          <div className="main-link-button">
+            <img src = "./assets/frontFacing/sign.png" style = {{width:'25vw'}}/>
+            <div id = "img-text">support</div>
+          </div>
+
+          <div className="main-link-button">
+            <img src = "./assets/frontFacing/creamery.png" style = {{width:'25vw'}}/>
+            <div id = "img-text">services</div>
+            </div>
+          </div>
     );
 }
 
 export const NewsletterSignup = (props) => {
     return(
         <div className="blurb-box">
-            <div className="header-title" style={{marginTop: '30px', fontSize: '40px'}}>
+                <div className="header-title" style={{marginTop: '30px', fontSize: '40px'}}>
                 Would you like to hear from us?
             </div>
+
             <div className="header-title" style={{marginTop: '90px', fontSize: '20px'}}>
                 Submit your email address for our newsletter
             </div>
@@ -69,28 +87,60 @@ export const CustomerTypeSelection = (props) => {
 
     return(
        <div className="blurb-box" style={{height: 'auto'}}>
-          <div className="customer-type-button" style={{backgroundColor: 'red'}}></div>
-          <div className="customer-type-button" style={{backgroundColor: 'green'}}></div>
-          <div className="customer-type-button" style={{backgroundColor: 'yellow'}}></div>
+        <div id = "customer-type-mask"></div>
+          <div className="customer-type-button">
+            <img src = "./assets/frontFacing/producer.jpg" width={window.innerWidth/3}/>
+
+            </div>
+          <div className="customer-type-button">
+            <img src = "./assets/frontFacing/martys.png" width={window.innerWidth/3}/></div>
+          <div className="customer-type-button">
+            <img src = "./assets/frontFacing/chefs.jpg" width={window.innerWidth/3}/></div>
          </div>
     );
-  
+
 }
 
+// Our Story section
 export const Bio = (props) => {
     return(
         <div id="bio" className="blurb-box">
-            <div id="bio-header">What is FoodLove?</div>
+        <div id="bio-picture">
+          <img src = "./assets/blueBerries.JPG"  width = "100%"/>
+        </div>
+            <div id="bio-header">This is our story</div>
+
             <div id="bio-body">
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-              </p>
-              <br/>
-              <p>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-              </p>
+              <div className = "section-header">
+                We love local food.
+              </div>
+                <div className = "bio-content">
+                  And we know you do too. That’s why we’re here.
+                  FoodLove is an easy-to-use online platform where
+                  local producers can sell to their wholesale consumers.
+                </div>
+              <div className = "section-header">
+                Our platform revolutionizes the supply chain.
+              </div>
+                <div className = "bio-content">
+                  We gather purveyors, artisans, and distributors of local
+                  food all in one place. With FoodLove, you can make new
+                  connections, have smoother transactions with current partners,
+                  and grow your operations with a new, expanded network.
+                </div>
+              <div className = "section-header">
+                At FoodLove, we’re here to empower the local food community.
+              </div>
+                <div className = "bio-content">
+                  Our mission is to support wholesome, responsible agriculture.
+                  We connect and engage others with the people behind our food
+                  as well as the environment that sustains it.
+                </div>
+              <div className = "section-header">
+                Eat food you can feel good about. Eat food you can love.
+              </div>
             </div>
-            <div id="bio-picture"></div>
+
         </div>
     );
 }
