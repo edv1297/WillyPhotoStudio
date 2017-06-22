ca28e570656c569bf4eac2302f0d613c7e5bf601
edv1297 <edv2@williams.edu>
2017-06-22 11:08:32 -0400

    swag

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,10 +9,8 @@ export const Footer = (props) => {
                 <div  className="footer-text"> All rights reserved © FoodLove Services LLC 2017</div>
                 <div className="footer-text"></div>
                 <div className="footer-text"> Designed with <span style={{color: '#b51e1e'}}> love</span> by our developers</div>
-<<<<<<< HEAD
+
                 <div className ="footer-text">Contact us <span style ={{textDecoration: 'underline'}}>mia@foodloveservices.com</span></div>
-=======
->>>>>>> 905f14ba985613c4ef9786776d1b4ad094997aa9
         </div>
     );
     }
