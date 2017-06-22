fdf99ccd4bdf87a24097b86b8d7e562b8b0bf904
michaelwong2 <michaelwongait@gmail.com>
2017-06-21 21:19:16 -0400

    did some stuff to sign in

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -13,52 +13,54 @@ export class SignIn extends React.Component{
 
     render(){
 
+        var body = null;
+
         if(this.state.registerClicked){
-            return (
+            body = (
                 <div>
-                    <Nav collapsable={false}/>
-                        <div className="navbar-replace"></div>
-                            <div className="header-title" id="contactus-page-title">Welcome to FoodLove</div>
-
-                            <div className="info-input-container">
-                                <input placeholder="First Name" className="contact-input"/></div>
-                            <div className="info-input-container">
-                                <input placeholder="Last Name" className="contact-input"/></div>
-                            <div className="info-input-container">
-                                <input placeholder="Email address" className="contact-input"/></div>
-                            <div className="info-input-container">
-                                <input placeholder="Password" className="contact-input"/></div>
-                            <div className="info-input-container">
-                                <input placeholder="Re-enter password" className="contact-input"/>
-                            </div>
-                            <div id="contact-submit">
-                                <button className="btn btn-primary" id = "login">Back</button>
-                                <button className="btn btn-primary" id = "register" onClick={this.onClick.bind(this)}
-                                >Confirm</button>
-                            </div>
-                        </div>
-                        )
-            }else{
-                return(
-                    <h>swag</h>
-
-                )
-            }
-
-        }
-
-            <div id="contact-submit">
-                <button className="btn btn-primary" id = "login">Login</button>
-                <button className="btn btn-primary" id = "register" onClick={this.onClick.bind(this)}
-                        >Need an account?</button>
+                    <div className="info-input-container">
+                        <input placeholder="First Name" className="contact-input"/>
+                    </div>
+                    <div className="info-input-container">
+                        <input placeholder="Last Name" className="contact-input"/>
+                    </div>
+                    <div className="info-input-container">
+                        <input placeholder="Email address" className="contact-input"/>
+                    </div>
+                    <div className="info-input-container">
+                        <input placeholder="Password" className="contact-input"/>
+                    </div>
+                    <div className="info-input-container">
+                        <input placeholder="Re-enter password" className="contact-input"/>
+                    </div>
+                    <div id="contact-submit">
+                        <button className="btn btn-primary" id = "login">Back</button>
+                        <button className="btn btn-primary" id = "register" onClick={this.onClick.bind(this)}>Confirm</button>
                     </div>
                 </div>
-            <div className="horizontal-spacer"></div>
-        <Footer/>
-        </div>
+                );
+            }else{
+                body = (
+                    <h>swag</h>
 
-        );
-    }
+                );
+            }
+
+            return(
+                <div>
+                    <Nav collapsable={false}/>
+                    <div className="navbar-replace"></div>
+                    <div className="header-title" id="contactus-page-title">Welcome to FoodLove</div>
+                    {body}
+                    <div id="contact-submit">
+                        <button className="btn btn-primary" id = "login">Login</button>
+                        <button className="btn btn-primary" id = "register" onClick={this.onClick.bind(this)}>Need an account?</button>
+                    </div>
+                    <div className="horizontal-spacer"></div>
+                    <Footer/>
+                </div>
+            );
+        }
 
     onClick(){
             this.setState({
